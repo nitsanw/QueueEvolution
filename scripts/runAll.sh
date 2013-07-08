634c1cf0fdd6c9726d615e81628b2a1257bf1130
nitsanw <nitsanw@yahoo.com>
2013-07-08 20:14:06 +0200

    fix JVM_OPTS

--- scripts/runAll.sh
+++ scripts/runAll.sh
@@ -1,15 +1,16 @@
-export JVM_OPTS=-XX:+UseNUMA -XX:+UseCondCardMark -XX:CompileThreshold=100000
-echo "Same Core"
+export JVM_OPTS='-XX:+UseNUMA -XX:+UseCondCardMark -XX:CompileThreshold=100000'
+echo summary,jvmopts,$JVM_OPTS
+echo "summary,Same Core"
 for IMPL in 3 31 41 42 43 44 45
 do
 	./repeatRunTestSC.sh $IMPL
 done
-echo "Cross Core"
+echo "summary,Cross Core"
 for IMPL in 3 31 41 42 43 44 45
 do 
         ./repeatRunTestCC.sh $IMPL
 done
-echo "Cross Socket"
+echo "summary,Cross Socket"
 for IMPL in 3 31 41 42 43 44 45
 do 
         ./repeatRunTestCS.sh $IMPL
--- scripts/runTest.sh
+++ scripts/runTest.sh
@@ -1,1 +1,1 @@
-java -server $JVM_OPTS -cp examples-1.0-SNAPSHOT.jar -Dscale=$1 uk.co.real_logic.queues.QueuePerfTest $1
+java -server $JVM_OPTS -cp examples-1.0-SNAPSHOT.jar uk.co.real_logic.queues.QueuePerfTest $1
